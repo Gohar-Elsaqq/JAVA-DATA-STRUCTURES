30c9b6d46e577d0fe1958a6a9bad7cbbab58d963
Gohar-Elsaqq <gohar.elsaqqa@gmail.com>
2024-11-16 17:27:14 +0200

    update The Arrays fix code

--- src/main/java/com/gohar/TheArrays.java
+++ src/main/java/com/gohar/TheArrays.java
@@ -4,34 +4,41 @@ import java.util.Arrays;
 
 public class TheArrays {
 
-    static String[] colors=new String[5];
+    static String[] colors = new String[5];
 
     public static void main(String[] args) {
-        String [] colors = new String[5];
-        colors[0]="red";
-       // get element
-        // System.out.println(Arrays.toString(colors));
-        colors[1]="black";
-        System.out.println(Arrays.toString(colors));
-        System.out.println(colors[0]);
-        System.out.println(colors[1]);
-        System.out.println(colors[2]);
-        System.out.println(colors[3]);
-        System.out.println(colors[4]);
-        colors[3]="yellow";
+        String[] colors = new String[5];
+        colors[0] = "red";
+        colors[1] = "black";
+        colors[3] = "yellow";
 
-        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>"+"<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
-        int[] numbers={100,200};
-        System.out.println("Coloers length : "+colors.length);
-        System.out.println("sort + "+">>>>>>>>>>>>>>>>>>>>>>>>>>>>>"+"<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
-        for (int i=0;i<colors.length;i++){
-            System.out.println(colors[i]);
+
+        System.out.println("Colors array: " + Arrays.toString(colors));
+
+
+        System.out.println("Colors length: " + colors.length);
+
+        System.out.println("Iterating using for-each loop:");
+        for (String color : colors) {
+            if (color != null) {
+                System.out.println(color);
+            } else {
+                System.out.println("No color assigned.");
+            }
         }
-        System.out.println("sort - "+">>>>>>>>>>>>>>>>>>>>>>>>>>>>>"+"<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
-        for (int i=colors.length-1;i>=0;i--){
-            System.out.println(colors[i]);
 
+
+        System.out.println("Iterating in reverse order:");
+        for (int i = colors.length - 1; i >= 0; i--) {
+            if (colors[i] != null) {
+                System.out.println(colors[i]);
+            } else {
+                System.out.println("No color assigned.");
+            }
         }
-    }
-    }
 
+
+        int[] numbers = {100, 200};
+        System.out.println("Numbers array: " + Arrays.toString(numbers));
+    }
+}
